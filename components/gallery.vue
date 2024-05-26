73a70f481c7f2b043ee63a3c5ecc70ab83ed89be
0xmatriksh <kishmat17@gmail.com>
2024-05-26 23:15:01 +0545

    feat: add setup for profile page

---
+++ components/gallery.vue
@@ -0,0 +1,18 @@
+<template>
+    <h2 class="text-xl font-semibold">Gallery</h2>
+    <div class="flex flex-col">
+        <div class="flex mx-10 w-[90%] space-x-10">
+            <img class="image-k" src="/images/kishmat01.jpeg" alt="img1">
+            <img class="image-k" src="/images/kishmat_bhattarai.jpeg" alt="img2">
+            <img class="w-[350px]" src="/images/kishmat-03.jpg" alt="img4">
+            <img class="image-k" src="/images/kishmat.jpeg" alt="img3">
+        </div>
+    </div>
+</template>
+
+<style>
+.image-k{
+    height: 350px;
+    object-fit: contain;
+}
+</style>
---
+++ components/profile.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h2 class="text-2xl font-semibold">Kishmat Bhattarai</h2>
+        <p class="text-lg">Software Developer</p>
+        <p>Graduated: <strong> Bachelor in Computer Science and Information Technology</strong></p>
+    </div>
+</template>
