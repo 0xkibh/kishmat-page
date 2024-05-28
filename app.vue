2f82ce2d48b0f789dcf9a48d3cecef254aae4a14
0xmatriksh <kishmat17@gmail.com>
2024-05-28 10:06:41 +0545

    feat: add education

--- app.vue
+++ app.vue
@@ -2,5 +2,6 @@
   <div class="py-5 px-5 flex flex-col space-y-6 bg-green-100  h-screen ">
     <Profile/>
     <Gallery/>
+    <Education/>
   </div>
 </template>
---
+++ components/education.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2 class="text-xl font-semibold">Education</h2>
+        <div class="flex flex-col space-y-4">
+            <div>
+                <p>Undergradudate - <strong> Bachelor of Science in Computer Science and Information Technology</strong></p>
+                <p>Tribhuvan University - Information Of Science and Technology <a class="text-blue-700 font-medium" href="https://iost.tu.edu.np/">(IOST)</a></p>
+            </div>
+            <div>
+                <p>High School - <strong>Science</strong></p>
+                <p>Kathmandu Model College <a class="text-blue-700 font-medium" href="https://ktmmodelcollege.edu.np/">(KMC)</a></p>
+            </div>
+            <div>
+                <p>Grade 9 and 10 - <strong>School Leaving Certificate(SLC)</strong></p>
+                <p>Mahakavi Devkota H.S.S <a class="text-blue-700 font-medium" href="https://www.collegenp.com/school/mahakavi-devkota-secondary-school-parasi/">(MDSS)</a></p>
+            </div>
+            <div>
+                <p>Nursery to Grade 8 - <strong>Primary and Lower Secondary</strong></p>
+                <p>Sakura Memorial Higher Secondary School <a class="text-blue-700 font-medium" href="https://www.facebook.com/p/Sakura-memorial-higher-secondary-school-100063716913647/">(Sakura)</a></p>
+            </div>
+        </div>
+    </div>
+</template>
