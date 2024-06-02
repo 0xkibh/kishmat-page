3aa5b9e511d428b78483347103b7e9a0a5d6f15b
0xmatriksh <kishmat17@gmail.com>
2024-06-02 12:06:28 +0545

    feat: update layout of page

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template >
-  <div class="py-5 px-5 flex flex-col space-y-6 bg-green-100  h-screen ">
+  <div class="py-5 px-5 flex flex-col space-y-6 bg-green-100  h-max w-full">
     <Profile/>
     <Gallery/>
     <Education/>
--- components/profile.vue
+++ components/profile.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h2 class="text-2xl font-semibold">Kishmat Bhattarai</h2>
+        <h2 class="text-xl md:text-2xl font-semibold">Kishmat Bhattarai</h2>
         <p class="text-lg">Software Developer</p>
         <p>Graduated: <strong> Bachelor in Computer Science and Information Technology</strong></p>
     </div>
