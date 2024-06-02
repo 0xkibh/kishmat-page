14b4bbd4560b1b4a27c8f061ee32552b764de22d
0xmatriksh <kishmat17@gmail.com>
2024-06-02 14:20:51 +0545

    refactor: screen height in md

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template >
-  <div class="py-5 px-5 flex flex-col space-y-6 bg-green-100  h-max w-full">
+  <div class="py-5 px-5 flex flex-col space-y-6 bg-green-100  h-max md:h-screen w-full">
     <Profile/>
     <Education/>
     <Gallery/>
