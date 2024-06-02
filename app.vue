e55342da8b32b9afa16b9e1aa3eebab091d156c6
0xmatriksh <kishmat17@gmail.com>
2024-06-02 12:42:56 +0545

    test: remove gallery

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template >
   <div class="py-5 px-5 flex flex-col space-y-6 bg-green-100  h-max w-full">
     <Profile/>
-    <Gallery/>
+    <!-- <Gallery/> -->
     <Education/>
   </div>
 </template>
