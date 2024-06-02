2acaee9bbbf5397ba64aee935dbbce14f19eba7b
0xmatriksh <kishmat17@gmail.com>
2024-06-02 13:42:51 +0545

    feat: use local path for all image

--- components/gallery.vue
+++ components/gallery.vue
@@ -2,10 +2,10 @@
     <h2 class="text-xl font-semibold">Gallery</h2>
     <div class="flex flex-col ">
         <div class="flex flex-col md:flex-row mx-10 w-[90%] space-x-10">
-            <NuxtImg class="image-k" src="https://raw.githubusercontent.com/0xmatriksh/kishmat-page/master/public/images/kishmat.jpeg" alt="img1"/>
-            <NuxtImg class="image-k" src="https://raw.githubusercontent.com/0xmatriksh/kishmat-page/master/public/images/kishmat.jpeg" alt="img2"/>
-            <NuxtImg class="w-[350px]" src="https://raw.githubusercontent.com/0xmatriksh/kishmat-page/master/public/images/kishmat-03.jpg" alt="img4"/>
-            <NuxtImg class="image-k" src="/images/kishmat01.jpeg" alt="img3"/>
+            <NuxtImg class="image-k" src="/images/kishmat01.jpeg" alt="img1"/>
+            <NuxtImg class="image-k" src="/images/kishmat_bhattarai.jpeg" alt="img2"/>
+            <NuxtImg class="w-[350px]" src="/images/kishmat-03.jpg" alt="img4"/>
+            <NuxtImg class="image-k" src="/images/kishmat.jpeg" alt="img3"/>
         </div>
     </div>
 </template>
