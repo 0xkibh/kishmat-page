ad5958ebbaa69e883bb2535ca69a60d7c0d46e40
0xmatriksh <kishmat17@gmail.com>
2024-06-02 14:17:57 +0545

    refactor: update image style

--- components/education.vue
+++ components/education.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h2 class="text-xl font-semibold">Education</h2>
+        <h2 class="text-lg md:text-xl font-semibold">Education</h2>
         <div class="flex flex-col space-y-4">
             <div>
                 <p>Undergradudate - <strong> Bachelor of Science in Computer Science and Information Technology</strong></p>
@@ -15,7 +15,7 @@
                 <p>Mahakavi Devkota H.S.S <a class="text-blue-700 font-medium" href="https://www.collegenp.com/school/mahakavi-devkota-secondary-school-parasi/">(MDSS)</a></p>
             </div>
             <div>
-                <p>Nursery to Grade 8 - <strong>Primary and Lower Secondary</strong></p>
+                <p>Starting to Grade 8 - <strong>Primary and Lower Secondary</strong></p>
                 <p>Sakura Memorial Higher Secondary School <a class="text-blue-700 font-medium" href="https://www.facebook.com/p/Sakura-memorial-higher-secondary-school-100063716913647/">(Sakura)</a></p>
             </div>
         </div>
--- components/gallery.vue
+++ components/gallery.vue
@@ -1,11 +1,11 @@
 <template>
-    <h2 class="text-xl font-semibold">Gallery</h2>
+    <h2 class="text-lg md:text-xl font-semibold">Gallery</h2>
     <div class="flex flex-col">
-        <div class="flex flex-col md:flex-row mx-1 md:mx-10 space-y-4 w-[90%] md:space-x-10">
-            <NuxtImg class="w-full md:w-[350px]" src="/images/kishmat01.jpeg" alt="img1"/>
-            <NuxtImg class="w-full md:w-[350px]" src="/images/kishmat_bhattarai.jpeg" alt="img2"/>
-            <NuxtImg class="w-[350px]" src="/images/kishmat-03.jpg" alt="img4"/>
-            <NuxtImg class="image-k" src="/images/kishmat.jpeg" alt="img3"/>
+        <div class="flex flex-col md:flex-row mx-1 md:mx-10 space-y-4 w-[90%] md:space-x-4 md:space-y-0">
+            <NuxtImg class="w-full md:h-[350px] md:object-contain" src="/images/kishmat01.jpeg" alt="img1"/>
+            <NuxtImg class="w-full md:h-[350px] md:object-contain" src="/images/kishmat_bhattarai.jpeg" alt="img2"/>
+            <NuxtImg class="w-full md:w-[350px]" src="/images/kishmat-03.jpg" alt="img4"/>
+            <NuxtImg class="w-full md:h-[350px] md:object-contain" src="/images/kishmat.jpeg" alt="img3"/>
         </div>
     </div>
 </template>
